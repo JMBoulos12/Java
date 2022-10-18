2a54345efaae1ed33aa1683f4f9a999e2a54c396
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2022-10-18 09:56:31 +0300

    Update Suit.java

--- Console/Blackjack/Suit.java
+++ Console/Blackjack/Suit.java
@@ -1,1 +1,12 @@
-
+/**
+ * An enum represnting the four possible suits of a card.
+ *
+ * @author Jean Marc Boulos
+ * @Since 18/10/2022
+ */
+public enum Suit {
+  Clubs,
+  Diamonds,
+  Spades,
+  Hearts,
+}
