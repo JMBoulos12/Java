ba1bdc9df401edc89017a9be07ed74fe8e869bd7
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2022-10-18 11:39:23 +0300

    Update Deck.java

--- Console/Blackjack/Deck.java
+++ Console/Blackjack/Deck.java
@@ -20,6 +20,16 @@ public class Deck {
   private int myCards;
   
   /**
+   * Constructor with a default of one deck (i.e. 52 cards) and no shuffling.
+   */
+  public Deck() {
+    
+    // all the other constructor, defining one deck without shuffling
+    this(1, false);
+    
+  }
+  
+  /**
    * Constructor that defines the number of decks (i.e., how many sets of 52
    * Cards are in the deck) and whether is should be shuffled.
    *
@@ -44,10 +54,79 @@ public class Deck {
         for (int n = 1: n <= 13; n++) {
          
           // add a new card tot he deck
-          this.myCards[c] = new Card()
+          this.myCards[c] = new Card(Suit.values()[s].n);
+          c++;
           
         }
       }
     }
+    
+    // shuffle, if necessary
+    if (shuffle) {
+      this.shuffle();
+    }
+    
+  }
+  
+  /**
+   * Shuffle deck bu randomly pairs of cards.
+   */
+  public void Shuffle() {
+    
+    // init random under generator
+    Random rng = new Random();
+    
+    // temporary card
+    Card temp;
+    
+    int j;
+    for (int i = 0; i < this.numCards; i++) {
+      
+      // get a random card j to swap i's value with
+      j = rng.nextInt(this.numCards);
+      
+      // do swap
+      temp = this.myCards[i];
+      this.myCards[i] = this.myCards[j];
+      this.myCards[j] = temp;
+      
+    }
+  }
+  
+  /**
+   * Deal the next card from the top of the deck.
+   *
+   * @return the dealt card
+   */
+  public Card dealNextCard() {
+
+    // get on the top card
+    Card top = this.myCards[0];
+    
+    // Shift all the subsequent cards to the left one index
+    for (int c = 1; c < this.numCards; c++) {
+      this.myCards[c-1] = this.myCards[c];
+    }
+    this.myCards[this.numCards-1] = null;
+    
+    // decrement the number of cards in your deck
+    this.numCards--;
+    
+    return top;
+  }  
+  
+  /**
+   * Print top cards in the deck.
+   *
+   * @param numToPrint    the number of cards from the top of the deck to
+   *                                              print
+   */
+  public printDeck(int numToPrint) {
+    
+    for (int c = 0; c < nu;ToPrint; c++) {
+      System.out.println("% 3d/%d %s\n" , c+1 , this.numCards , 
+                         this.myCards[c].toString());
+    }
+    System.out.printf("\t\t[%d other]", this.numCards-numToPrint);
   }
 }
