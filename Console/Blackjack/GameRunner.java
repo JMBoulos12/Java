377a79abecacaa14b8ba60dec54e8d6193d1ca40
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2022-10-18 11:50:02 +0300

    Update GameRunner.java

--- Console/Blackjack/GameRunner.java
+++ Console/Blackjack/GameRunner.java
@@ -3,7 +3,7 @@ public class GameRunner {
   
   public static void main(String[] args) {
     
-    Deck myDeck = new Deck();
+    Deck myDeck = new Deck(1, true);
     myDeck.printDeck(24);
   }
   
