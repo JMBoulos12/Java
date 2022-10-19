03de52e1789667580e84ce75ce503c7aa1fef2f9
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2022-10-19 15:06:13 +0300

    Update Bank.java

--- Console/ATM/Bank.java
+++ Console/ATM/Bank.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Bank {
   
@@ -9,12 +10,85 @@ public class Bank {
   
   private ArrayList<Account> accounts;
   
+  /**
+   * Generate a new universally unique ID for a user.
+   * @return the uuid
+   */
   public String getNewUserUUID() {
+    
+    // inits
+    String uuid;
+    Random rng = new Random();
+    int len = 6;
+    boolean nonUnique;
+    
+    // Continue looping until we get a unique ID
+    do {
+      
+        // Generate the number
+        uuid = "";
+        for (int c = 0; c < len; c++) {
+          uuid += ((Integer)rng.nextInt(10)).toString();
+        }
+      
+      // check to make sure it's unique
+      nonUnique = false
+      For (User u : this.users) {
+        if (uuid.compareTo(u.getUUID()) == 0) {
+          nonUnique = true;
+          break;
+        }
+      }
+      
+    } while (nonUnique);
+    
+    return uuid;
   
   }
   
+  /**
+   * Generate a new universally unique ID for an account
+   * @return the uuid
+   */
   public String getNewAccountUUID() {
+    
+    // inits
+    String uuid;
+    Random rng = new Random();
+    int len = 10;
+    boolean nonUnique;
+    
+    // Continue looping until we get a unique ID
+    do {
+      
+        // Generate the number
+      uuid = "";
+      for (int c = 0; c < len; c++) {
+        uuid += ((Integer)rng.nextInt(10)).toString();
+      }
+
+    // check to make sure it's unique
+    nonUnique = false
+    For (Account a : this.accounts) {
+      if (uuid.compareTo(a.getUUID()) == 0) {
+        nonUnique = true;
+        break;
+      }
+    }
+  } while (nonUnique);
+
+    
+  return uuid;
+    
+    
+}
   
+  /**
+   * add on account
+   * @param anAcct  the account to add
+   */ 
+  public void addAccount() {
+    this.accounts.add(onAcct); 
   }
   
 }
