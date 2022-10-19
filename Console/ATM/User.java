5428289c6667b1188fa79e798e0a12e8f65291bb
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2022-10-19 11:36:34 +0300

    Update User.java

--- Console/ATM/User.java
+++ Console/ATM/User.java
@@ -1,1 +1,71 @@
 
+import java.util.ArrayList;
+import java.security.MessageDigest;
+
+
+public class User {
+  
+  /**
+   * The first name of the user. 
+   */
+  private String firstName;
+  
+  /**
+   * The last name of the user.
+   */
+  private String lastName;
+  
+  /**
+   * The ID number of the user.
+   */
+  private String uuid;
+  
+  /**
+   * The MD5 hash of the user's pin number.
+   */
+  private byte pinHash[];
+  
+  /**
+   * The list of documents for this user.
+   */
+  private ArrayList<Account> accounts;
+  
+  /**
+   * Create a new user
+   * @param firstName the user's first name
+   * @param lastName  the user's lirst name
+   * @param pin       the user's account pin number
+   * @param theBank   the Bank object that the user is a customer of 
+   */
+  public User(String firstName, String pin, Bank theBank) {
+    
+    // set user's name
+    this.firstName = firstName;
+    this.lastName = lastName;
+    
+    // store the pin's MD5 hash, rather than the original value,
+    // for security reasons
+    try {
+          MessageDigest md = MessageDigest.getInstance("MD5");
+          this.pinHash = md.diges(pin.getBytes());
+    } catch (NoSuchAlgorithmException e) {
+      System.err.println("Error, Caught: NoSuchAlgorithmException");
+      e.printStackTrace();
+      System.exit(1);
+    }
+    
+    // get a new, unique universal ID for the user
+    this.uuid = theBank.getNewUserUUID();
+    
+    // create empty list of accounts
+    this.accounts = new ArrayList<Account>();
+    
+    // print log message
+    System.out.printf("New user %s, %s with ID %s created. \n", LastName, firstName, this.uuid);
+    
+  }
+  
+  public void addAccount(Account onAcct) {
+    this.accounts.add(onAcct);
+  }
+}
