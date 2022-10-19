d862c523035357746738a789f5cd61a329924995
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2022-10-19 11:36:39 +0300

    Update Transaction.java

--- Console/ATM/Transaction.java
+++ Console/ATM/Transaction.java
@@ -1,1 +1,26 @@
 
+import java.util.Date;
+
+public class Transaction {
+  
+  /**
+   * The amount of this transaction.
+   */
+  private double account;
+  
+  /**
+   * The time and date of this transaction.
+   */ 
+  private Date timestamp;
+  
+  /**
+   * A memo for this transaction.
+   */
+  private String memo;
+  
+  /**
+   * The account in which the transaction was performed.
+   */
+  private Account inAccount;
+  
+}
