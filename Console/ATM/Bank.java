3c92f611700e0f95ca570c96b1a1e35b561cff4b
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2022-10-19 11:36:45 +0300

    Update Bank.java

--- Console/ATM/Bank.java
+++ Console/ATM/Bank.java
@@ -1,1 +1,20 @@
 
+import java.util.ArrayList;
+
+public class Bank {
+  
+  private String name;
+  
+  private ArrayList<User> users;
+  
+  private ArrayList<Account> accounts;
+  
+  public String getNewUserUUID() {
+  
+  }
+  
+  public String getNewAccountUUID() {
+  
+  }
+  
+}
