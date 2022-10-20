e3adc699f645420ebea970ca6d230e6e3cdb7a53
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2022-10-20 14:47:25 +0300

    Update Transaction.java

--- Console/ATM/Transaction.java
+++ Console/ATM/Transaction.java
@@ -23,4 +23,33 @@ public class Transaction {
    */
   private Account inAccount;
   
+  /**
+   * Create a new Transaction
+   * @param amount    the amount transacted
+   * @param inAccount the account the transaction beloongs to
+   */
+  public Transaction(double amount, Account inAccount) {
+    
+    this.amount = amount;
+    this.inAccount = inAccount;
+    this.timestamp = new Date();
+    this.memo = '';
+    
+  }
+  
+  /**
+   * Create a new transaction
+   * @param amount      the amount
+   * @param memo        the memo for the transaction
+   * @param inAccount   the account the transaction belongs to
+   */
+  public Transaction(double amount, String menu, Account inAccount) {
+    
+    // call the two-arg constructor first
+    this(amount, inAccount);
+    
+    // set the memo
+    this.memo = memo;
+  
+  }
 }
