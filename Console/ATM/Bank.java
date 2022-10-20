358c646e55258c26f8697f811f24f7bfa24f3d19
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2022-10-20 14:47:28 +0300

    Update Bank.java

--- Console/ATM/Bank.java
+++ Console/ATM/Bank.java
@@ -11,6 +11,18 @@ public class Bank {
   private ArrayList<Account> accounts;
   
   /**
+   * Create a new Bank objects with empty lists of users and accounts
+   * @param name  the name of the bank
+   */
+  public Bank(String name) {
+    
+    this.name = name;
+    this.users = new ArrayList<User>();
+    this.accounts = new ArrayList<Acount>();
+    
+  }
+  
+  /**
    * Generate a new universally unique ID for a user.
    * @return the uuid
    */
@@ -80,15 +92,65 @@ public class Bank {
     
   return uuid;
     
-    
 }
+  /**
+   * Add on account
+   * @param anAcct  the account to add
+   */
+  public void addAccount() {
+    this.accounts.add(anAcct);
+  }
   
   /**
-   * add on account
-   * @param anAcct  the account to add
-   */ 
-  public void addAccount() {
-    this.accounts.add(onAcct); 
+   * Create a new user of the bank
+   * @param firstName  the user's first name
+   * @param lastName   the user's last name
+   * @param pin        the user's pin
+   * @return           the new User object
+   */
+  public User addUser(String firstName, String lastName, String pin) {
+  
+    // create a new User object and add it to our list
+    User newUser = new User(firstName, lastName, pin, this);
+    this.users.add(newUser);
+    
+    // create a savings account for the user 
+    Account newAccount = new Account("Savings", newUser, this);
+    newUser.addAccount(newAccount);
+    this.addAccount.add(newAccount);
+      
+    return newUser;
+  }
+  
+  /**
+   * Get the User object associated with a particular userID and pin, if they
+   * are valid
+   * @param userID      the UUID of the user to log in
+   * @param pin         the pin of the user
+   * @return            it is not
+   *
+   */
+  public User userLogin(String userID, String pin) {
+    
+    // Search through list of users
+    for(User u : this.users) {
+      
+      // checl user ID is correct
+      if (u.getUUID().compareTo(userID) == 0 && u.validatePin(pin)) {
+        return u;
+      }
+    }
+    
+    // if we haven't found the user or have an incorrect pin
+    return null;
+  }
+  
+  /**
+   * Get the name of the bank
+   * @return the name of the bank
+   */
+  public String getName() {
+    return this.name;
   }
   
 }
