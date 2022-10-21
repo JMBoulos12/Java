dbacce5b192a53c17c28a60c8f97adea270c689a
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2022-10-21 12:25:37 +0300

    Update Account.java

--- Console/ATM/Account.java
+++ Console/ATM/Account.java
@@ -51,6 +51,51 @@ public class Account {
     return this.uuid;
   }
   
+  /**
+   * Get summary line for the account
+   * @return the string summary
+   */
+  public String getSummaryLine() {
+    
+    // get the account's balance
+    double balance = this.getBalance();
+    
+    // format the summary line, depending on the whether the balance is
+    // negative
+    if (balance >= 0) {
+          return String.format("%s : $%.02f : %s", this.uuid, balance,
+                            this.name);
+      } else {
+          return String.format("%s : $(%.02f) : %s", this.uuid, balance,
+                              this.name);
+    } 
+  }
+  
+  /**
+   * Get the balance of this account by adding the amounts of the transactions
+   * @return the balance value
+   */
+  public double getBalance() {
+    
+    double balance = 0;
+    for (Transaction t : this.transactions) {
+      balance += t.getAmount();
+    }
+    return balance;
+  }
+  
+  /**
+   * Print the transaction history of the account
+   */
+  public void printTransHistory() {
+    
+    System.out.printf("\nTransaction history for account %s\n", this.uuid);
+    for (int t = this.transactions.size()-1; t >= 0; t--) {
+        System.out.printf(this.transaction.get(t).getSummaryLine());
+    }
+    System.out.println(); 
+  }
+    
 }
   
   
