b4287f7024f84c0908666f647e93099510e1247e
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2022-10-21 15:59:58 +0300

    Update User.java

--- Console/ATM/User.java
+++ Console/ATM/User.java
@@ -115,9 +115,9 @@ public class User {
    */
   public void printAccountsSummary() {
     
-    System.out.printf("\n\n%s's accounts summary", this.firstName);
+    System.out.printf("\n\n%s's accounts summary\n", this.firstName);
     for (int a = 0; a < this.accounts.size(); a++) {
-      System.out.printf("%d) %s\n", a+1,
+      System.out.printf("   %d) %s\n", a+1,
                        this.accounts.get(a).getSummaryLine());
     }
     System.out.println();
