8b3ca7856e719b279fc5272c105655828691c212
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2022-11-10 15:08:21 +0200

    Create Main.java

---
+++ JavaFX/GUI/Handle User Events/Main.java
@@ -0,0 +1,44 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/* 10/11/2022 */
+
+public class Main extends Appication implements EventHandler<ActionEvent> {
+  
+  Button button;
+  
+  public static void main(String[] args){
+    launch(args); 
+  }
+  
+  @override
+  public static start(Stage primaryStage) throws Exception{
+    primaryStage.setTitle("Title of the windows");
+    button = new Button();
+    button.setText("Click me"); 
+    button.setOnAction(this);
+    
+    StackPane layout = new StackPane();
+    layout.getChildren().add(button);
+    
+    Scene scene = new Scene(layout, 300, 250);
+    primaryStage..getScene(scene);
+    primaryStage.show();
+  }
+  
+  @Override
+  public void handle(ActionEvent event) {
+    if(event.getSource() == button) {
+      System.out.println("Ohh, did you saw that bird?");
+    }
+    if(event.getSource() == button2) {
+      System.out.println("You clicked button 2");
+    }
+  }
+  
+}
