6d415fb90d402d7094a3924d2cfa5f7ca2067743
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2022-11-11 12:07:08 +0200

    Create Main.java

---
+++ JavaFX/GUI/5- Creating Alert Boxes/Main.java
@@ -0,0 +1,29 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+  
+  Stage window;
+  Button button;
+  
+  public static void main(String[] args) {
+    launch(args);
+  }
+  
+  @Override
+  public void start(Stage primaryStage) {
+    window = primaryStage;
+    window.setTitle("JMB");
+    
+    button = new Button("Click Me");
+    
+    StackPane layout = new StackPane();
+    layout.getChildren().add(button);
+    Scene scene = new Scene(layout, 300, 250);
+    window.setScene(scene);
+    window.show();
+  }
+}
