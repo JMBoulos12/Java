5e263df384a89f97cff1c4ab8786ce054e1aef14
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2022-11-14 10:27:28 +0200

    Create Main.java

---
+++ JavaFX/GUI/7- Closing the Program Properly/Main.java
@@ -0,0 +1,40 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+  
+  Stage window;
+  Button button;
+  
+  public static void main(String[] args) {
+    launch(args);
+  }
+  
+  @Override
+  public void start(Stage primaryStage) {
+    window = primaryStage;
+    window.setTitle("JMB");
+    
+    window.setOnCloseRequest(e -> {
+      e.consume();
+      closeProgram();
+    });
+    
+    button = new Button("Close Program");
+    button.setOnAction(e -> closeProgram());
+    
+    StackPane layout = new StackPane();
+    layout.getChildren().add(button);
+    Scene scene = new Scene(layout, 300, 250);
+    window.setScene(scene);
+    window.show();
+  }
+  
+  private void closeProgram() {
+    Boolean answer = confirmBox.display("titles", "Sure you want to exit");
+    if(answer)
+      window.close();
+  }
