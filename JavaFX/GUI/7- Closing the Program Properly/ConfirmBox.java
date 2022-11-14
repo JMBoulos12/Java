4e66b5c20bdb1e64ab3d3631b600b2f9aada8029
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2022-11-14 10:28:55 +0200

    Create ConfirmBox.java

---
+++ JavaFX/GUI/7- Closing the Program Properly/ConfirmBox.java
@@ -0,0 +1,42 @@
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
+
+public class ConfirmBox {
+  
+  static boolean answer;
+  
+  public static boolean display(String title, String message) {
+    Stage message = new Stage();
+    window.initModality(Modality.APPLICATION_MODAL);
+    window.setTitle(title);
+    window.setMinWidth(250);
+    Label label = new Label();
+    label.setText(message);
+    
+    // create two buttons
+    Button yeaButton = new Button("Yes");
+    Button noButton  = new Button("No");
+    
+    yesButton.setOnAction(e -> {
+      answer = true;
+      window.close();
+    });
+    noButton.setOnAction(e -> {
+      answer = false;
+      window.close();
+    });
+    
+    VBox layout = new VBox(10);
+    layout.getChildren().addAll(label, yesButton, noButton);
+    layout.setAlignment(POS.CENTER);
+    Scene scene = new Scene(layout);
+    window.setScene(scene);
+    window.showAndWait();
+    
+    return answer;
+    }
+  
+}
