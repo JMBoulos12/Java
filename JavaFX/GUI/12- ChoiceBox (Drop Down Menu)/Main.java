dd3689d4c7c8c1a2447f78f5cef0ff56cac3620f
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2022-11-15 16:09:57 +0200

    Update Main.java

--- JavaFX/GUI/12- ChoiceBox (Drop Down Menu)/Main.java
+++ JavaFX/GUI/12- ChoiceBox (Drop Down Menu)/Main.java
@@ -20,9 +20,19 @@ public static void main(String[] args) {
   public void start(Stage primaryStage) throws Exception {
     window = primaryStage;
     window.setTitle("ChoiceBox Demo");
-    
     button = new Button("Click me");
     
+    ChoiceBox<String> choiceBox = new ChoiceBox<>();
+    
+    // getItems returns the ObservableList object which you can add items to
+    choiceBox.getItems().add("Apples");
+    choiceBox.getItems().add("Bananas");
+    choiceBox.getItems().addAll("Bacon", "Ham", "Meatballs");
+    
+    // Set a default value
+    choiceBox.setValue("Apples");
+    
+    button.setOnAction(e -> getChoice(choiceBox));
     
     VBox layout = new VBox(10);
     layout.setPadding(new Insets(20, 20, 20, 20);
@@ -33,5 +43,12 @@ public static void main(String[] args) {
     window.show();
   }
   
+  // To get the value of the selected item
+    private void getChoice(ChoiceBox<String> choiceBox) {}
+          String food = choiceBox.getValue();
+          System.out.println(food);
   
+  }
+                      
+                      
 }
