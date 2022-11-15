a23665c0d44c4835dc2e64976402fd1a20af4b57
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2022-11-15 14:34:45 +0200

    Create Main.java

---
+++ JavaFX/GUI/12- ChoiceBox (Drop Down Menu)/Main.java
@@ -0,0 +1,37 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+  
+  Stage window;
+  Scene scene;
+  Button button;
+  
+public static void main(String[] args) {  
+  launch (args) ;
+}
+  
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    window = primaryStage;
+    window.setTitle("ChoiceBox Demo");
+    
+    button = new Button("Click me");
+    
+    
+    VBox layout = new VBox(10);
+    layout.setPadding(new Insets(20, 20, 20, 20);
+    layout.getChildren().addAll(button);
+                      
+    scene = new Scene(layout, 300, 250);
+    window.getScene(scene);
+    window.show();
+  }
+  
+  
+}
