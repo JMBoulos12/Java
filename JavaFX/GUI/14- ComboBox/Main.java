a18a63216216f29eb5639b6881ac5f91fc4edb82
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2022-11-16 10:39:00 +0200

    Create Main.java

---
+++ JavaFX/GUI/14- ComboBox/Main.java
@@ -0,0 +1,55 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+  
+  Stage window;
+  Scene scene;
+  Button button;
+  ComboBox<String> comboBox;
+  
+  public static void main(String[] args) {
+    launch(args);
+  }
+  
+  @Override
+  public static start(Stage primaryStage) throws Exception {
+    window = primaryStage;
+    window.setTitle("ComboBox Demo");
+    button = new Button("Submit");
+    
+    comboBox = new ComboBox<>();
+    comboBox.getItems().addAll (
+            "Good will Hunting",
+            "St. Charbel",
+            "Blackhat"
+    );
+    
+    comboBox.setPromptText("What is your favorite movie?");
+    
+    comboBox.setEditable(true);
+    
+    button.setOnAction(e -> printMovie());
+    comboBox.setOnAction(e -> System.out.println("User selected: " + comboBox.getValue() ) );
+    
+    VBox layout = new VBox(10);
+    layout.setPadding(new Inset(20, 20, 20, 20));
+    layout.getChildren().addAll(comboBox, button);
+    
+    scene = new Scene(layout, 300, 250);
+    window.setScene(scene);
+    window.show();
+  }
+  
+  // print out a movie
+  private void printMovie() {
+    System.out.println(comboBox.getValue());
+  }
+  
+  
+}
