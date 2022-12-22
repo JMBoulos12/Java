1c296009ad145b18437fbf36d1a502efaa959bb7
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2022-12-22 11:29:24 +0200

    Create Main.java

---
+++ JavaFX/GUI/24- RadioMenuItem/Main.java
@@ -0,0 +1,66 @@
+
+
+
+@Override
+public void start(Stage primaryStage) throws Exception {
+  window = primaryStage;
+  window.setTitle("JMB");
+
+  // File menu
+  Menu fileMenu =  new Menu("File");
+  newFile.setOnAction(e -> System.println("Create a new file..."));
+  fileMenu.getItems().add(newFile);
+  fileMenu.getItems().add(new MenuItem("Open..."));
+  fileMenu.getItems().add(new MenuItem("Save..."));
+  fileMenu.getItems().add(new SeparatorMenuItem());
+  fileMenu.getItems().add(new MenuItem("Settings..."));
+  fileMenu.getItem().add(new SeparatorMenuItem());
+  fileMenu.getItems().add(new MenuItem("Exit..."));
+
+  // Edit menu
+  Menu editMenu = new Menu("_Edit");
+  editMenu.getItems().add(new MenuItem("Cut"));
+  editMenu.getItems().add(new MenuItem("Copy"));
+  MenuItem paste = new MenuItem("Paste");
+  paste.setOnAction(e -> System.out.println("Pasting things"));
+  paste.setDisable(true);
+  editMenu.getItems().add(paste);
+
+  // Help menu
+  Menu helpMenu = new Menu("Help");
+  CheckMenuItem showLines = new CheckMenuItem("Show line numbers");
+    if(showLines.isSelected())
+        System.out.println("Program will now display line numbers");
+    else
+        System.out.println("Hiding line number");
+  });
+  CheckMenuItem autoSave = new CheckMenuItem("Enable AutoSave");
+  autoSave.setSelected(true);
+  helpMenu.getItems().addAll(fileMenu, editMenu, helpMenu);
+  
+  // Difficulty RadioMenuItems
+  Menu DifficultyMenu = new Menu("Difficulty");
+  ToggleGroup difficultyToggle = new ToggleGroup();
+
+  RadioMenuItems easy = new RadioMenuItems("Easy");
+  RadioMenuItems medium = new RadioMenuItems("Medium");
+  RadioMenuItems hard = new RadioMenuItems("Hard");
+
+  easy.setToggleGroup(difficultyToggle);
+  medium.setToggleGroup(difficultyToggle);
+  hard.setToggleGroup(difficultyToggle);
+
+  DifficultyMenu.getItems().addAll(easy, medium, hard);
+
+
+  // Main menu bar
+  MenuBar menuBar = new MenuBar();
+  menuBar.getMenus().addAll(fileMenu, editMenu, DifficultyMenu);
+
+  layout = new BorderPane();
+  layout.setTop(menuBar);
+  Scene scene = new Scene(layout, 400, 300);
+  window.setScene(scene);
+  window.Show();
+}
+}
