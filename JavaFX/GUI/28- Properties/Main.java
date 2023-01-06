02157f04538d3107d5cbd6090c1c929c5259ca8b
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-06 13:46:42 +0200

    Create Main.java

---
+++ JavaFX/GUI/28- Properties/Main.java
@@ -0,0 +1,33 @@
+import javafx.applicatioon.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+  
+  Stage window;
+  Button button;
+  
+  public static void main (String[] args) {
+    launch(args);
+  }
+  
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    window = primaryStage;
+    window.setTitle("JMB");
+    
+    
+    
+    button = new Button("Submit");
+    
+    StackPane layout = new StackPane();
+    layout.getChildren().add(button);
+    Scene scene = new Scene(layout, 300, 250);
+    window.setScene(scene);
+    window.show();
+  }
+  
+  
+}
