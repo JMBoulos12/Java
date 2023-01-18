6fc39479bee0bc0022f439f103492844f8837c7c
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-18 09:05:58 +0200

    Update LinearSearch.java

--- Exercices/Easy/LinearSearch.java
+++ Exercices/Easy/LinearSearch.java
@@ -1,5 +1,8 @@
 
 /*
+  Write a method that returns the index of the first occurrence of given integer in a list.
+  Assume that the index of the first element in the list is zero.
+  If the number does not exist return -1.
   18-January-2023
 */
 
