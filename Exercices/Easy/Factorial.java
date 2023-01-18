f640411eba810a718d5814b920c61d4122f70777
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-18 09:05:19 +0200

    Update Factorial.java

--- Exercices/Easy/Factorial.java
+++ Exercices/Easy/Factorial.java
@@ -1,6 +1,9 @@
 
 
 /*
+  Write a method that calculates the factorial of a given number.
+  Factorial is the product of all positive integers less than or equal to n. For example, factorial(4) = 4x3x2x1 = 24.
+  TIP: To make it more interesting, try to do it recursively.
   18-January-2023
 */
 
