b0a84fb7c1c02a02f3bbfbc56f9cf27ca9e85dfd
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-19 11:13:45 +0200

    Create ReturnSomething.java

---
+++ Exercices/Very Easy/ReturnSomething.java
@@ -0,0 +1,23 @@
+
+
+
+/*
+  Write a function that returns the string "something" joined with a space " " and the given argument a.
+
+  Examples
+  giveMeSomething("is better than nothing") ➞ "something is better than nothing"
+
+  giveMeSomething("Bob Jane") ➞ "something Bob Jane"
+
+  giveMeSomething("something") ➞ "something something"
+  Notes
+  Assume an input is given.
+  
+  19-January-2023
+*/
+
+public class Program {
+  public static String giveMeSomething(String a) {
+    return "something " + a;
+  }
+}
