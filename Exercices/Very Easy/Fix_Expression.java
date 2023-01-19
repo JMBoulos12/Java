ab1d72fe4dcba6bdefbaa97b140ec45b036f27d3
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-19 11:17:32 +0200

    Create Fix_Expression.java

---
+++ Exercices/Very Easy/Fix_Expression.java
@@ -0,0 +1,23 @@
+
+
+
+/*
+  Fix the code in the Code tab so the function returns true if and only if x is equal to 7.
+
+  Examples  :
+  Challenge.isSeven(4) ➞ false
+
+  Challenge.isSeven(9) ➞ false
+
+  Challenge.isSeven(7) ➞ true
+  Notes
+  The bug can be hard to find, so look closely!
+  
+  19-January-2023
+*/
+
+public final class Challenge {
+  public static boolean isSeven(int x) {
+    return x == 7;
+  }
+}
