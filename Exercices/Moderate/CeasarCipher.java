8f84da89ff5a6dd56fc7b3b7de77b0bec4d54078
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-19 08:46:44 +0200

    Create CeasarCipher.java

---
+++ Exercices/Moderate/CeasarCipher.java
@@ -0,0 +1,22 @@
+
+
+/*
+  The Ceasar cipher is a basic encryption technique used by Julius Ceasar to securely communicate with his generals. 
+  Each letter is replaced by another letter N positions down the english alphabet. 
+  For example, for a rotation of 5, the letter 'c' would be replaced by an 'h'. In case of a 'z', the alphabet rotates and it is transformed into a 'd'.
+  Implement a decoder for the Ceasar cipher where N = 5.
+  TIP: Use code.toCharArray() to get an array of characters.
+  19-January-2023
+*/
+
+public String decode(String code) {
+  String decoded = "";
+  int lastCharValue = 'z';
+  int alphabetLength = 'z' - 'a' + 1;
+  for (char character: code.toCharArray()) {
+    int charNoRotation = character + 5;
+    int charValue = charNoRotation < lastCharValue ? charNoRotation : charNoRotation - alphabetLength;
+    decoded += (char) charValue;
+  }
+  return decoded;
+}
