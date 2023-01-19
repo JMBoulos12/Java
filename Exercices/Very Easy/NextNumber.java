870f338211becaff4766ef63e298cec4a147b54c
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-19 09:57:38 +0200

    Create NextNumber.java

---
+++ Exercices/Very Easy/NextNumber.java
@@ -0,0 +1,20 @@
+
+
+
+/*
+  Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+  
+  Examples :
+  addition(0) ➞ 1
+
+  addition(9) ➞ 10
+
+  addition(-3) ➞ -2
+  19-January-2023
+*/
+
+public class Challenge {
+  public static int addition(int num) {
+    return ++num;
+  }
+}
