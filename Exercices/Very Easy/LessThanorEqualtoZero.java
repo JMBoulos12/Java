7cda19821558c2d440fb51a537677cef51309c61
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-19 10:31:38 +0200

    Create LessThanorEqualtoZero.java

---
+++ Exercices/Very Easy/LessThanorEqualtoZero.java
@@ -0,0 +1,21 @@
+
+
+
+/*
+  Create a method that takes an integer as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+
+  Examples
+  lessThanOrEqualToZero(5) ➞ false
+
+  lessThanOrEqualToZero(0) ➞ true
+
+  lessThanOrEqualToZero(-2) ➞ true
+  
+  19-January-2023
+*/
+
+public class Program {
+  public static boolean lessThanOrEqualToZero(int num) {
+    return num <= 0;
+  }
+}
