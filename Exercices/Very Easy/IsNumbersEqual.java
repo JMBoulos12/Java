a3cd3cfd4858383d0b9b38dfbf0b1e0ee0492d48
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-19 10:01:18 +0200

    Create IsNumbersEqual.java

---
+++ Exercices/Very Easy/IsNumbersEqual.java
@@ -0,0 +1,23 @@
+
+
+
+/*
+  Create a function that returns true when x is equal to y; otherwise return false.
+
+  Examples
+  isSameNum(4, 8) ➞ false
+
+  isSameNum(2, 2) ➞  true
+
+  isSameNum(42, 32) ➞  false
+  Notes
+  Don't forget to return the result.
+  
+  19-January-2023
+*/
+
+public class Program {
+  public static boolean isSameNum(int x, int y) {
+    return x == y;
+  }
+}
