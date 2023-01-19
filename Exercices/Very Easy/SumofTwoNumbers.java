9a1784137c1a17ae292323adcc7587e36f27b096
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-19 09:43:50 +0200

    Create SumofTwoNumbers.java

---
+++ Exercices/Very Easy/SumofTwoNumbers.java
@@ -0,0 +1,21 @@
+
+
+
+/*
+  Create a method that takes two integers as arguments and returns their sum.
+
+  Examples :
+  SumOfTwoNumbers(3, 2) ➞ 5
+
+  SumOfTwoNumbers(-3, -6) ➞ -9
+
+  SumOfTwoNumbers(7, 3) ➞ 10
+  
+  19-January-2023
+*/
+
+public class SumOfTwoNumbers {
+  public static int sum(int a, int b) {
+    return a + b;
+  }
+}
