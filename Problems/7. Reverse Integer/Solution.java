f9edd2c0234d62d48ad88779ed080a22e409278f
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-01-31 08:52:13 +0200

    Update Solution.java

--- Problems/7. Reverse Integer/Solution.java
+++ Problems/7. Reverse Integer/Solution.java
@@ -27,12 +27,15 @@
 */
 
 
- public int reverse(int x) {
-   String ans = x < 0 ? new StringBuilder(String.valueOf(-x)).append("-").reverse().toString()
-     : new StringBuilder(String.valueOf(x)).reverse().toString();
-   try {
-     return Integer.parseInt(ans);
-   } catch (Exception e) {
-     return 0;
-   }
- }
+public int reverse(int x) {
+  long res = 0;
+  while (x != 0) {
+    res = res * 10 + x % 10;
+    x = x / 10;
+  }     
+  if (res < Integer.MIN_VALUE || res > Integer.MAX_VALUE) {
+    return 0;
+  } else {
+    return (int)res;
+  }
+}
