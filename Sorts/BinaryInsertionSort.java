7ce67e150c31ac832ad3f778917a13203e5a0d1f
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-02-05 14:56:42 +0200

    Create BinaryInsertionSort.java

---
+++ Sorts/BinaryInsertionSort.java
@@ -0,0 +1,37 @@
+
+
+
+
+/*
+  Binary Insertion Sort method 
+  
+  05-February-2023
+*/
+
+package com.thealgorithms.sorts;
+
+public class BinaryInsertionSort {
+  
+  public int[] binaryInsertSort(int[] array) {
+    for (int i = 1; i < array.length; i++) {
+      int temp = array[i];
+      int low = 0;
+      int high = i - 1;
+      
+      while (low <= high) {
+        int mid = (low + high) / 2;
+        if (temp < array[mid]) {
+          high = mid - 1;
+        } else {
+          low = mid + 1;
+        }
+      }
+      
+      for (int j = i; j >= low + 1; j--) {
+        array[j] = array[j - 1];
+      }
+        array[low] = temp;
+    }
+    return array;
+  }
+}
