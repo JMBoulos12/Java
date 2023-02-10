d2921a3c883012c5f10dd38d7b778a607b76c18b
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-02-10 09:57:32 +0200

    Create Utils.java

---
+++ ciphers/a5/Utils.java
@@ -0,0 +1,31 @@
+
+
+
+
+/*
+  Source http://www.java2s.com/example/java-utility-method/bitset/increment-bitset-bits-int-size-9fd84.html
+  package com.java2s;
+  License from project: Open Source License 
+  
+  10-February-2023
+*/
+
+
+package com.jmboulos.ciphers.a5;
+
+import java.util.BitSet;
+
+public class Utils {
+
+    public static boolean increment(BitSet bits, int size) {
+        int i = size - 1;
+        while (i >= 0 && bits.get(i)) {
+            bits.set(i--, false);/*from w w w  . j a  v a  2s  .c o  m*/
+        }
+        if (i < 0) {
+            return false;
+        }
+        bits.set(i, true);
+        return true;
+    }
+}
