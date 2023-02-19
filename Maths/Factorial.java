f70943b89a8ed799bc0e3ac88ac1cc04d5bfe87f
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-02-19 18:28:00 +0200

    Create Factorial.java

---
+++ Maths/Factorial.java
@@ -0,0 +1,32 @@
+
+
+
+
+/*
+ * Calculate factorial N using iteration 
+  
+  19-February-2023
+*/
+
+
+package com.jmboulos.maths;
+
+public class Factorial {
+
+    /* Driver Code */
+    public static void main(String[] args) {
+        assert factorial(0) == 1;
+        assert factorial(1) == 1;
+        assert factorial(5) == 120;
+        assert factorial(10) == 3628800;
+    }
+  
+    public static long factorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("number is negative");
+        }
+        long factorial = 1;
+        for (int i = 1; i <= n; factorial *= i, ++i);
+        return factorial;
+    }
+}
