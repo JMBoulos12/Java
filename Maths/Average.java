bca9e9fc3cac67cff7de6d4b25b66300e3738f9e
Jean Marc Boulos <65892342+JMBoulos12@users.noreply.github.com>
2023-02-19 12:07:13 +0200

    Create Average.java

---
+++ Maths/Average.java
@@ -0,0 +1,44 @@
+
+
+
+
+/**
+  * Calculate average of a list of numbers.
+  
+  19-February-2023
+*/
+
+
+package com.jmboulos.maths;
+
+public class Average {
+
+    /**
+     * Calculate average of a list of numbers
+     *
+     * @param numbers array to store numbers
+     * @return mean of given numbers
+     */
+    public static double average(double[] numbers) {
+        double sum = 0;
+        for (double number : numbers) {
+            sum += number;
+        }
+        return sum / numbers.length;
+    }
+
+    /**
+     * find average value of int array
+     *
+     * @param numbers the array contains element and the sum does not excess long
+     *                value limit
+     * @return average value
+     */
+    public static int average(int[] numbers) {
+        long sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return (int) (sum / numbers.length);
+    }
+}
