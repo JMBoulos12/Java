fa0c2672cd68da2a7a01d6fcc1d69193d50f32ab
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-02-20 08:42:32 +0200

    Create HeronsFormula.java

---
+++ Maths/HeronsFormula.java
@@ -0,0 +1,25 @@
+
+
+
+
+/*
+ *  Find the area of a triangle using only side lengths
+  
+  20-February-2023
+*/
+
+
+package com.jmboulos.maths;
+
+public class HeronsFormula {
+
+    public static double Herons(int s1, int s2, int s3) {
+        double a = s1;
+        double b = s2;
+        double c = s3;
+        double s = (a + b + c) / 2.0;
+        double area = 0;
+        area = Math.sqrt((s) * (s - a) * (s - b) * (s - c));
+        return area;
+    }
+}
