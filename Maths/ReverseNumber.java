456443d0370b3bb745f7ae57d2804f146bbb6fb0
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-02-21 09:30:15 +0200

    Create ReverseNumber.java

---
+++ Maths/ReverseNumber.java
@@ -0,0 +1,39 @@
+
+
+
+
+/*
+  21-February-2023
+*/
+
+
+package com.jmboulos.maths;
+
+import java.lang.IllegalStateException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+    public static void main(String[] args) {
+        int number;
+        int reverse = 0;
+
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a number:");
+            number = sc.nextInt();
+        } catch (NoSuchElementException | IllegalStateException e) {
+            System.out.println("ERROR: Invalid input");
+            return;
+        }
+
+        while (number != 0) {
+            int remainder = number % 10;
+
+            reverse = reverse * 10 + remainder;
+            number = number / 10;
+        }
+
+        System.out.println("The reverse of the given number is: " + reverse);
+    }
+}
