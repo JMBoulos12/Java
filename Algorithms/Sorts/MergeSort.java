80872f46873f0383a14cf30b98fc4ae1f5d39479
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-03 11:55:08 +0200

    Update and rename Sorts/MergeSort.java to Algorithms/Sorts/MergeSort.java

--- Algorithms/Sorts/MergeSort.java
+++ Algorithms/Sorts/MergeSort.java
@@ -8,9 +8,9 @@
   06-February-2023
 */
 
-package com.thealgorithms.sorts;
+package com.jmboulos.sorts;
 
-import static com.thealgorithms.sorts.SortUtils.less;
+import static com.jmboulos.sorts.SortUtils.less;
 
 class MergeSort implements SortAlgorithm {
   
