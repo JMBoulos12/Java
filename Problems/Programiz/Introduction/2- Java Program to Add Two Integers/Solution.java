c30eeb55b5b0e8005852353f52136a69bb8f1848
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-09 09:08:20 +0200

    Create Solution.java

---
+++ Problems/Programiz/Introduction/2- Java Program to Add Two Integers/Solution.java
@@ -0,0 +1,33 @@
+
+
+
+
+/*
+  In this program, two integers 10 and 20 are stored in integer variables first and second respectively.
+  
+  Then, first and second are added using the + operator, and its result is stored in another variable sum.
+  
+  Finally, sum is printed on the screen using println() function.
+  
+  09-March-2023
+*/
+
+
+class Main {
+
+  public static void main(String[] args) {
+    
+    int first = 10;
+    int second = 20;
+
+    // add two numbers
+    int sum = first + second;
+    System.out.println(first + " + " + second + " = "  + sum);
+  }
+}
+
+/*
+Output:
+Enter two numbers : 10 20
+The sum is        : 30
+*/
