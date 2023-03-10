337698dfabf40f5c622a9364331d56a7b610426e
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-10 10:13:48 +0200

    Create Main.java

---
+++ Problems/Programiz/3- Convert int type variables to char/2- int to char by using forDigit()/Main.java
@@ -0,0 +1,41 @@
+
+
+
+
+/*
+  We can also use the forDigit() method of the Character class to convert the int type variable into char type.
+  
+  We have used the forDigit() method converts the specified int value into char value.
+
+  Here, 10 and 16 are radix values for decimal and hexadecimal numbers respectively. 
+  That is, if the int value is between 0 to 9, we use 10 as radix value, if the int value is between 0 to 15, we use 16, and so on.
+
+  10-March-2023
+*/
+
+
+class Main {
+  public static void main(String[] args) {
+
+    // create int variables
+    int num1 = 1;
+    int num2 = 13;
+
+    // convert int to char
+    // for value between 0-9
+    char a = Character.forDigit(num1, 10);
+
+    // for value between 0-9
+    char b = Character.forDigit(num2, 16);
+
+    // print value
+    System.out.println(a);    // 1
+    System.out.println(b);    // d
+  }
+}
+
+/*
+Notice the expression,
+
+char a = Character.forDigit(num1, 10);
+*/
