3a7d4e8c52e6b472129cbbf85ce904f99139bf16
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-16 08:29:41 +0200

    Create Main.java

---
+++ Problems/Programiz/3- Decision Making and Loop/13- Calculate the Power of a Number/3- using pow() function/Main.java
@@ -0,0 +1,23 @@
+
+
+
+
+/*
+  Output  :
+  Answer = 0.012345679012345678
+  
+  In this program, we use Java's Math.pow() function to calculate the power of the given base.
+
+  16-March-2023
+*/
+
+
+class Main {
+  public static void main(String[] args) {
+
+    int base = 3, exponent = -4;
+    double result = Math.pow(base, exponent);
+
+    System.out.println("Answer = " + result);
+  }
+}
