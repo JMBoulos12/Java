63881c7f21a116da50c0ecb841184bb252d19f10
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-03-16 11:41:48 +0200

    Create Main.java

---
+++ Problems/Programiz/3- Decision Making and Loop/23- Create Pyramid and Pattern/2- using numbers/Main.java
@@ -0,0 +1,28 @@
+
+
+
+
+/*
+  1
+  1 2
+  1 2 3
+  1 2 3 4
+  1 2 3 4 5
+
+  16-March-2023
+*/
+
+
+public class Main {
+
+  public static void main(String[] args) {
+    int rows = 5;
+
+    for (int i = 1; i <= rows; ++i) {
+      for (int j = 1; j <= i; ++j) {
+        System.out.print(j + " ");
+      }
+      System.out.println();
+    }
+  }
+}
