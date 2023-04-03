e12ae014d33d6bee83342acca601ac6adc4d530c
Jean Marc BOULOS <65892342+JMBoulos12@users.noreply.github.com>
2023-04-03 08:52:31 +0300

    Create helloWorld.java

---
+++ Problems/3- Exercism/1- Hello world/helloWorld.java
@@ -0,0 +1,56 @@
+
+
+
+
+/*
+  Instructions
+  The classical introductory exercise. Just say "Hello, World!".
+
+  "Hello, World!" is the traditional first program for beginning programming in a new language or environment.
+
+  The objectives are simple:
+
+  * Write a function that returns the string "Hello, World!".
+  * Run the test suite and make sure that it succeeds.
+  * Submit your solution and check it at the website.
+  
+  If everything goes well, you will be ready to fetch your first real exercise.
+
+  Since this is your first Java exercise, we've included a detailed Tutorial that guides you through your solution. Check it out for tips and assistance!
+
+  * Introduction
+  * Exercise Structure
+  * Solving "Hello, World!"
+  * Step 1: Fix the solution
+  * Step 2: Run the tests
+  * Step 3: Submitting your first iteration
+  * Next steps
+  
+  Welcome to the first exercise on the Java track!
+
+  This document is a step-by-step guide to solving this exercise. The instructions should work equally well on Windows, macOS and Linux.
+
+  Even if you are already familiar with Java basics, stepping through this guide may be helpful as it will help you understand the output from the tool, 
+  Gradle, that we use to compile and test our code, as well as introduce some of the patterns common to all exercises in this track.
+
+  When you fetch a new Java exercise, you will receive:
+
+  one or more test files (always). These live in the src/test/java directory 
+  and define a set of tests that our solution must satisfy before we can safely declare that it is working.
+  one or more starter files (initially). 
+  If provided, these live in the src/main/java directory and define shells of the classes you will need in order to solve the current problem.
+  
+  Output  :
+  Hello, World!
+  
+  03-April-2023
+*/
+
+
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
